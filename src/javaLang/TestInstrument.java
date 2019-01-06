c26d1e420a95030391600595aede3810fc0c67cf
chu.liangcai <790704125@qq.com>
2019-01-06 15:36:10 +0800

    Default Changelist

--- src/javaLang/TestInstrument.java
+++ src/javaLang/TestInstrument.java
@@ -4,7 +4,7 @@ import java.lang.instrument.Instrumentation;
 
 public class TestInstrument {
 
-    public static void premain(String agentArgs, Instrumentation inst){
+    public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("occur in pre main");
     }
 }
